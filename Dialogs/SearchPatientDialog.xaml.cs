8b984d44ac512f2f88e3509eed626cf9615f67f8
youngprogrammerAbdo <aboooooooodm@gmail.com>
2025-10-31 18:29:46 +0200

    Add missing pages

---
+++ Dialogs/SearchPatientDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClinicManagementSystem.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SearchPatientDialog.xaml
+    /// </summary>
+    public partial class SearchPatientDialog : Window
+    {
+        public SearchPatientDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
